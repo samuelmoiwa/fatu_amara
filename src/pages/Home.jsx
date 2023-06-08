fb4d99be8620afa0f0cbc9b86d6863a612a3974f
samuelmoiwa <samuelmoiwa@gmail.com>
2023-06-08 17:59:50 +0000

    add Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function home() {
+  return (
+    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-20">
+      <p className="text-3xl text-gray-700 font-bold mb-5">
+        Welcome!
+      </p>
+      <p className="text-gray-500 text-lg">
+        To Fatu Amara profile site, currently under development
+      </p>
+    </div>
+  )
+}
+
+export default home
+

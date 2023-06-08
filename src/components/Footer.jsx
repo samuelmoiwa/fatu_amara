a72463a24915d2aaa6fe6e37a5ced8226765d204
samuelmoiwa <samuelmoiwa@gmail.com>
2023-06-08 17:59:01 +0000

    add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,62 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+import logo from '../images/logo.png'
+
+function Footer() {
+  return (
+    <footer className="bg-gray-100 py-8">
+      <div className="container mx-auto px-4">
+        <div className="flex justify-center items-center">
+          <img src={logo} alt="Logo" className="h-16 w-52 mr-2" />
+        </div>
+        <div className="flex flex-col md:flex-row justify-center items-center mt-4">
+          <div className="md:mr-8">
+            <h4 className="text-gray-800 uppercase font-semibold">Contact Us</h4>
+            <p className="text-gray-600 mt-2">
+              Address: 123 Street, usa, United State
+            </p>
+            <p className="text-gray-600 mt-1">Phone: +1 00 000 000</p>
+            <p className="text-gray-600 mt-1">Email: amarafatue@gmail.com</p>
+          </div>
+          <div className="mt-4 md:mt-0">
+            <h4 className="text-gray-800 uppercase font-semibold">Social Media</h4>
+            <div className="flex mt-2">
+              <a
+                href="#"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-600 hover:text-gray-800 mr-3"
+              >
+                Facebook
+              </a>
+              <a
+                href="#"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-600 hover:text-gray-800 mr-3"
+              >
+                Twitter
+              </a>
+              <a
+                href="#"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-gray-600 hover:text-gray-800"
+              >
+                Instagram
+              </a>
+            </div>
+          </div>
+        </div>
+        <hr className="border-gray-600 my-6" />
+        <div className="text-center text-gray-500 text-sm">
+          &copy; {new Date().getFullYear()} Fatu Amara. All rights reserved.
+          ~ Design By <span className='hover:text-gray-800'> <a href='https://github.com/samuelmoiwa'
+          target="_blank" rel="noopener noreferrer"> Abdulai Samuel Moiwa </a></span>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;

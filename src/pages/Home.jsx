ce47343e5eb7adead0b80934a3139caffb2b1267
samuelmoiwa <samuelmoiwa@gmail.com>
2023-06-14 14:51:27 +0000

    update home.jsx add about section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useEffect } from 'react';
 import fatu from '../images/fatu.png'
+import fatu_about from '../images/fatu_about_me.png'
 import AwardsCard from '../components/AwardsCard';
 
 
@@ -84,6 +86,85 @@ function Home() {
           AWARDS & FELLOWSHIPS
         </h2>
         < AwardsCard />
+      </section>
+
+      <section class="bg-white border-b py-8" id='about'>
+        <div class="container max-w-5xl mx-auto m-8">
+          <div className="text-center py-2 flex flex-col items-center justify-center">
+            <p className="text-2xl font-bold leading-none sm:text-3xl
+          text-purple-header my-6'">
+              About me
+            </p>
+            <div className="w-16 h-1 bg-blue-800 mt-2" data-aos="fade-right"> </div>
+          </div>
+          <div class="w-full mb-4">
+            <div class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
+          </div>
+
+          <div class="flex flex-wrap flex-col-reverse sm:flex-row" data-aos="fade-up">
+            <div class="w-full sm:w-1/2 p-6">
+              <p class="text-gray-600 mb-8 text-justify">
+                Fatu Amara graduated with honors and was awarded the American Chemical Society (ACS)
+                Accredited BS in Chemistry from CUNY College of Staten Island (CSI).
+                <br />
+                <br />
+                As President of the Student Government, she represented the 14,000 student body as an Executive
+                Board member to five college boards, managed the $400,000 budget, and developed the Student
+                Government Internship Program.
+                <br />
+                <br />
+                As a Watson Fellow, she worked on public health issues in the US, Rwanda, and the UK. Her work
+                includes program management, qualitative and quantitative research analysis, literature reviews,
+                and scientific journal writing on SARS, Ebola, and COVID-19 issues.
+                <br />
+                <br />
+                She plans to work at the intersection of health and policy to lead large scale programs that
+                ensure the well-being of vulnerable communities.
+              </p>
+
+              <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+                <a rel="noopener noreferrer" href="#"
+                  className="px-4 py-2 font-semibold rounded
+                  dark:bg-purple dark:text-white
+                  transform hover:scale-110 transition-transform duration-300"
+                >
+                  Education
+                </a>
+
+                <a rel="noopener noreferrer" href="#"
+                  className="px-4 py-2 font-semibold rounded
+                  dark:bg-purple dark:text-white
+                  transform hover:scale-110 transition-transform duration-300"
+                >
+                  Project
+                </a>
+
+                <a rel="noopener noreferrer" href="#"
+                  className="px-4 py-2 font-semibold rounded
+                  dark:bg-purple dark:text-white
+                  transform hover:scale-110 transition-transform duration-300"
+                >
+                  Coordinator
+                </a>
+
+                <a rel="noopener noreferrer" href="#"
+                  className="px-4 py-2 font-semibold rounded
+                  dark:bg-purple dark:text-white
+                  transform hover:scale-110 transition-transform duration-300"
+                >
+                  Researcher
+                </a>
+
+              </div>
+            </div>
+            <div class="w-full sm:w-1/2 p-6 sm:order-first">
+              <img class="w-full rounded shadow-md sm:rounded-none sm:shadow-none"
+                src={fatu_about} alt="image"
+              />
+            </div>
+          </div>
+
+        </div>
       </section>
 
       <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-20">

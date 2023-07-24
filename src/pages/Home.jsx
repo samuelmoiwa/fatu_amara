e864c29f07693aa6dce997e9a696df59396535c0
samuelmoiwa <samuelmoiwa@gmail.com>
2023-07-24 08:05:47 -0700

    update home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -384,18 +384,11 @@ function Home() {
       </section>
 
       <section className="flex flex-col justify-center
-      items-center w-full py-6 sm:px-2 bg-gray-100"
+      items-center w-full py-6 sm:px-2 bg-white"
       id="contact"
       >
         <ContactUs />
       </section>
-
-      <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-20">
-        <p className="text-3xl text-gray-700 font-bold mb-5">Welcome!</p>
-        <p className="text-gray-500 text-lg">
-          To Fatu Amara profile site, currently under development
-        </p>
-      </div>
     </>
   );
 }

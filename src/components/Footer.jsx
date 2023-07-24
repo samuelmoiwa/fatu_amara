64d81f9d2ee7df5d4abcfa3b7ed90993d0495283
samuelmoiwa <samuelmoiwa@gmail.com>
2023-07-24 08:05:28 -0700

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,10 +11,10 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row justify-center items-center mt-4">
           <div className="md:mr-8">
-            <h4 className="text-gray-800 uppercase font-semibold">Contact Us</h4>
-            <p className="text-gray-600 mt-2">
+            <h4 className="text-gray-800 uppercase font-semibold">Contact Me</h4>
+            {/* <p className="text-gray-600 mt-2">
               Address: 123 Street, usa, United State
-            </p>
+            </p> */}
             <p className="text-gray-600 mt-1">Phone: +1 00 000 000</p>
             <p className="text-gray-600 mt-1">Email: amarafatue@gmail.com</p>
           </div>

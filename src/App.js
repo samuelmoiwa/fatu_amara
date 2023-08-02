05a8c05406d186919f72f5ac6cfb67665bf19b50
samuelmoiwa <samuelmoiwa@gmail.com>
2023-08-02 14:34:41 -0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
   return (
     <>
       <NavBar />
-
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/readMore" element={<HealthReadMore />} />
